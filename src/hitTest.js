8e6cc3eca5d8bf889b17cd936776925e6fed1482
Nolan Hays <haysnolan@hotmail.com>
2025-03-20 10:51:45 -0400

    Attempt #1 at fixing the game over screen

--- src/hitTest.js
+++ src/hitTest.js
@@ -1,29 +1,29 @@
-import * as THREE from "three";
-import { metadata as rows } from "./components/map";
-import { player, position } from "./components/Player";
+// import * as THREE from "three";
+// import { metadata as rows } from "./components/map";
+// import { player, position } from "./components/Player";
 
-const resultDOM = document.getElementById("result-container");
-const finalScoreDOM = document.getElementById("final-score");
+// const resultDOM = document.getElementById("result-container");
+// const finalScoreDOM = document.getElementById("final-score");
 
-export function hitTest() {
-    const row = rows[position.currentRow - 1];
-    if (!row) return;
+// export function hitTest() {
+//     const row = rows[position.currentRow - 1];
+//     if (!row) return;
 
-    if (row.type === "car" || row.type === "truck") {
-        const playerBoundingBox = new THREE.Box3();
-        playerBoundingBox.setFromObject(player);
+//     if (row.type === "car" || row.type === "truck") {
+//         const playerBoundingBox = new THREE.Box3();
+//         playerBoundingBox.setFromObject(player);
 
-        row.vehicles.forEach(({ ref }) => {
-            if (!ref) throw Error("VEHICLE REFERENCE IS MISSING");
+//         row.vehicles.forEach(({ ref }) => {
+//             if (!ref) throw Error("VEHICLE REFERENCE IS MISSING");
 
-            const vehicleBoundingBox = new THREE.Box3();
-            vehicleBoundingBox.setFromObject(ref);
+//             const vehicleBoundingBox = new THREE.Box3();
+//             vehicleBoundingBox.setFromObject(ref);
 
-            if (playerBoundingBox.intersectsBox(vehicleBoundingBox)) {
-                if (!resultDOM || !finalScoreDOM) return;
-                resultDOM.style.visibility = "visible";
-                finalScoreDOM.innerText = position.currentRow.toString();
-            }
-        });
-    }
-}
+//             if (playerBoundingBox.intersectsBox(vehicleBoundingBox)) {
+//                 if (!resultDOM || !finalScoreDOM) return;
+//                 resultDOM.style.visibility = "visible";
+//                 finalScoreDOM.innerText = position.currentRow.toString();
+//             }
+//         });
+//     }
+// }
